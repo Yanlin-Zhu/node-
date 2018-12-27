ab179a5b57c9f16c44d13b65877d20fe7cb50474
zz <13613655791@163.com>
2018-12-28 00:09:47 +0800

    fs

--- 04fs/fs.js
+++ 04fs/fs.js
@@ -72,3 +72,28 @@ fs.rmdir('css', (err) => {
   }
 })
 
+/**
+ * filename (String) 文件名称
+ * data (String | Buffer) 将要写入的内容，可以是字符串或者 buffer 数据。
+ * · encoding (String) 可选。默认 'utf-8'，当 data 是 buffer 时，该值应该为 ignored。
+ * · mode (Number) 文件读写权限，默认 438。
+ * · flag (String) 默认值 'w'。
+ * callback { Function } 回调，传递一个异常参数 err。
+ */
+fs.writeFile('index.js', 'Hello zz', (err) => {
+  if(err) {
+    console.log(err);
+    return false;
+  } else {
+    console.log('写入成功！');
+  }
+})
+
+fs.appendFile('index.js', '这段文本是要追加的内容', (err) => {
+  if(err) {
+    console.log(err);
+    return false;
+  } else {
+    console.log("追加成功");
+  }
+})
